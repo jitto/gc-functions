b9cfd88e6481f27ff7180466758ddd935b80e856
jitto <jitto@users.noreply.github.com>
2016-06-16 20:04:43 -0500

    Update index.js

--- hello-gcf/index.js
+++ hello-gcf/index.js
@@ -1,4 +1,4 @@
-exports.hellohttp = function (context, data) {
+exports.helloworld = function (context, data) {
   // Use the success argument to send data back to the caller
   context.success('My GCF Function: ' + data.message);
 };
